971870be61bbd6824b30a4791d90edbabf9522b4
moonctp <moonctp24@gmail.com>
2024-08-11 18:16:02 +0900

    init setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
-import "~/styles/globals.css";
+import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="@/public/joyfun-festival-icon.png" />
+        <title>joyful-festival</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
