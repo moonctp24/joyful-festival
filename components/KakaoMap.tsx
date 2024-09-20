fc4a59795e3d24f6b711a77320c17436c9987139
moonctp <moonctp24@gmail.com>
2024-09-20 11:56:33 +0900

    kakao map test drawing

--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -1,15 +1,50 @@
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const KakaoMap = () => {
+  const positions = [
+    {
+      title: "카카오",
+      latlng: { lat: 33.450705, lng: 126.570677 },
+    },
+    {
+      title: "생태연못",
+      latlng: { lat: 33.450936, lng: 126.569477 },
+    },
+    {
+      title: "텃밭",
+      latlng: { lat: 33.450879, lng: 126.56994 },
+    },
+    {
+      title: "근린공원",
+      latlng: { lat: 33.451393, lng: 126.570738 },
+    },
+  ];
+
   return (
     <div>
       <Map
-        center={{ lat: 33.5563, lng: 126.79581 }}
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
         style={{ width: "500px", height: "360px" }}
+        level={3} // 지도의 확대 레벨
       >
-        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-          <div style={{ color: "#000" }}>Hello World!</div>
-        </MapMarker>
+        {positions?.map((position, index) => (
+          <MapMarker
+            key={`${position.title}-${position.latlng}`}
+            position={position.latlng} // 마커를 표시할 위치
+            image={{
+              src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커이미지의 주소입니다
+              size: {
+                width: 24,
+                height: 35,
+              }, // 마커이미지의 크기입니다
+            }}
+            title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          />
+        ))}
       </Map>
     </div>
   );
