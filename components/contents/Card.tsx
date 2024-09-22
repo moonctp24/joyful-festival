64e1aeda7253d0bb8266c8383bd31a6ffbd6af14
moonctp <moonctp24@gmail.com>
2024-09-22 13:30:09 +0900

    Card Component Dummy Layout

--- components/contents/Card.tsx
+++ components/contents/Card.tsx
@@ -1,5 +1,23 @@
-const Card = () => {
-  return <div className="w-[200px] h-[200px] border">card</div>;
+import { FESTIVAL_DEFAULT_IMAGE } from "@/constants/CONST";
+import Image from "next/image";
+
+type CardPropsType = {
+  festivalImg: string;
+};
+const Card = (props: CardPropsType) => {
+  const festivalImg = props.festivalImg;
+  return (
+    <>
+      <Image
+        width={50}
+        height={50}
+        src={festivalImg.length > 0 ? festivalImg : FESTIVAL_DEFAULT_IMAGE}
+        alt={"festivalImg"}
+        style={{ width: "170px", height: "170px" }}
+      ></Image>
+      <p className="w-[119px] h-7 text-xs text-left text-black">4/11 ~ 4/17</p>
+    </>
+  );
 };
 
 export default Card;
--- components/main/resultArea/ResultListComp.tsx
+++ components/main/resultArea/ResultListComp.tsx
@@ -1,21 +1,24 @@
 import Card from "@/components/contents/Card";
 
 const ResultListComp = () => {
+  const festivalImg =
+    "https://lh3.googleusercontent.com/proxy/hmB710GCcPlxDLDU5BmhAlwsQTWR42OP_ANkM4W2CsuL4WF-_CYxYIINGqFBepNM00QC1l2UvLKzu6yb_02tRq54iRcl5SYw6YB-hIszGL4SgvsTXozMUA";
+
   return (
     <>
-      <div className="flex">
-        <Card />
-        <Card />
+      <div>
+        <Card festivalImg={festivalImg} />
+        <Card festivalImg="" />
       </div>
 
       <div className="flex">
-        <Card />
-        <Card />
+        <Card festivalImg="" />
+        <Card festivalImg="" />
       </div>
 
       <div className="flex">
-        <Card />
-        <Card />
+        <Card festivalImg="" />
+        <Card festivalImg="" />
       </div>
     </>
   );
