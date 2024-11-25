a4fff81daf268eaa51bb3a3459beb214c3ff32e1
moonctp <moonctp24@gmail.com>
2024-11-25 21:45:38 +0900

    축제 상세 위치 데이터 get parameter setting

--- pages/api/getFestivalList.tsx
+++ pages/api/getFestivalList.tsx
@@ -7,9 +7,18 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   if (req.method === "GET") {
+    // get 통신 parameter 세팅
+    let getParamSetting = "?";
+    // eslint-disable-next-line prefer-const
+    for (let q of Object.entries(req.query)) {
+      getParamSetting += q[0] + "=" + q[1] + "&";
+    }
+    getParamSetting = getParamSetting.slice(0, -1);
+    //`${BASE_URL}${url}${getParamSetting}`
     try {
       const response = await axios.get(
-        "http://ec2-3-34-40-99.ap-northeast-2.compute.amazonaws.com/festivals",
+        `http://ec2-3-34-40-99.ap-northeast-2.compute.amazonaws.com/festivals${getParamSetting}`,
+        { headers: req.headers },
       );
       res.status(200).json(response.data);
     } catch (error: any) {
