58e75193e88c7ce147aad280588c97fa6548cc93
moonctp <moonctp24@gmail.com>
2024-12-18 12:03:50 +0900

    축제 검색 API 더미적용

--- components/main/MainComp.tsx
+++ components/main/MainComp.tsx
@@ -6,6 +6,7 @@ import DropBoxComp from "../comm/select/DropBoxComp";
 import MainTabComp from "./MainTabComp";
 import ResultComp from "./resultArea/ResultComp";
 import InputComp from "../comm/inputComp/InputComp";
+import axios from "axios";
 
 type RootState = {
   nowTab: {
@@ -17,7 +18,17 @@ const MainComp: NextPage = () => {
   const nowTab = useSelector((state: RootState) => state.nowTab.nowTabNum);
 
   const ingYNList = ["개최중여부 선택", "개최중", "개최예정"];
-  const monthList = ["개최시기 선택", "1월", "2월", "3월", "4월"];
+  const monthList = [
+    "개최시기 선택",
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+  ];
   const [regionList, setRegionList] = useState([
     "지역 선택",
     "서울",
@@ -40,10 +51,26 @@ const MainComp: NextPage = () => {
    */
   const getInputTxt = (inputTxt: string) => {
     console.log(inputTxt);
-    // const nameParam = {
-    //   name: inputTxt,
-    // };
-    // fetchData("get", "/api/medicineMng/getMdcnList", nameParam, true); // 의약품 리스트 조회 통신
+  };
+
+  /**
+   * 축제 검색 통신
+   */
+  const searchFestList = () => {
+    const sendData = {
+      ing: "01", // 개최중여부 -  00:전체, 01:개최중, 02:개최예정
+      month: "3", // 개최시기 - 0:전체, 1:1월, 2:2월, ..., 12:12월
+      region: "부산", // 지역명
+      word: "불꽃축제", // 검색어
+    };
+    axios
+      .get("/api/getFestivalList", { params: sendData })
+      .then((response) => {
+        console.log("맵 초기 데이터 조회 결과");
+        console.log(response);
+        // getNewPingList(response);
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
@@ -60,7 +87,9 @@ const MainComp: NextPage = () => {
           <InputComp defaultPlaceholder={"축제명"} getInputTxt={getInputTxt} />
         </div>
         <div className="space30 webHidden" />
-        <button className="h-10 webNavBttn">검색</button>
+        <button className="h-10 webNavBttn" onClick={searchFestList}>
+          검색
+        </button>
       </div>
       <div className="space30" />
       <ResultComp />
