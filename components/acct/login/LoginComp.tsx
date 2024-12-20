f4bb4e208ca56a1f0499d3a29d58778a045ae9f8
moonctp <moonctp24@gmail.com>
2024-12-20 19:38:33 +0900

    create login page

---
+++ components/acct/login/LoginComp.tsx
@@ -0,0 +1,58 @@
+import { alertAction } from "@/store/modal/alert-slice";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+const LoginComp = () => {
+  // const [emailInput, setEmailInput] = useState("test@email.com");
+  // const [passwordInput, setPasswordInput] = useState("test123!!");
+
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  const emailChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailInput(e.target.value);
+  };
+
+  const passwordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordInput(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="w-full h-[1080px] overflow-hidden bg-[#00c1a6]">
+        <div className="absolute top-1/2	left-1/2	-mt-[200px] -ml-[250px] w-[500px] h-[400px] bg-white/75">
+          <div className="absolute top-1/2	left-1/2	-mt-[150px] -ml-[170px]  flex w-[340px] h-[300px] flex-col justify-start items-center gap-5">
+            <p className="text-[40px] text-left text-[#00c1a6]">로그인</p>
+            <input
+              type="text"
+              placeholder="이메일을 입력해 주세요"
+              value={emailInput}
+              data-key="emailInput"
+              onChange={emailChangeHandler}
+            />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              value={passwordInput}
+              data-key="passwordInput"
+              onChange={passwordChangeHandler}
+              // onKeyDown={autoLoginHandler}
+            />
+            <button className="h-11">로그인</button>
+            <p
+              className="w-[340px] h-3.5 text-xs text-right text-black makePointer"
+              onClick={() => {
+                // router.push("/nauth/findpw");
+              }}
+            >
+              비밀번호 찾기
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginComp;
