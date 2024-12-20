0489b82b93d36abab75034f47d914d331aab098e
moonctp <moonctp24@gmail.com>
2024-12-20 21:50:25 +0900

    festival select option list CONST

--- constants/CONST.tsx
+++ constants/CONST.tsx
@@ -2,3 +2,32 @@
  * default Imgae
  */
 export const FESTIVAL_DEFAULT_IMAGE = "/images/joyful-festival-icon.png";
+
+/**
+ * festival ing Code List
+ */
+export const ING_YN_LIST = [
+  { value: "00", text: "개최중여부 선택" },
+  { value: "01", text: "개최중" },
+  { value: "02", text: "개최예정" },
+  { value: "03", text: "개최종료" },
+];
+
+/**
+ * Month List
+ */
+export const MONTH_LIST = [
+  { value: 0, text: "개최시기 선택" },
+  { value: 1, text: "1월" },
+  { value: 2, text: "2월" },
+  { value: 3, text: "3월" },
+  { value: 4, text: "4월" },
+  { value: 5, text: "5월" },
+  { value: 6, text: "6월" },
+  { value: 7, text: "7월" },
+  { value: 8, text: "8월" },
+  { value: 9, text: "9월" },
+  { value: 10, text: "10월" },
+  { value: 11, text: "11월" },
+  { value: 12, text: "12월" },
+];
