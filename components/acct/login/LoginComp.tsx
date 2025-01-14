647c5ec3216138387bed36e7a502ddc2b294f15a
moonctp <moonctp24@gmail.com>
2025-01-14 22:05:43 +0900

    login API Logic

--- components/acct/login/LoginComp.tsx
+++ components/acct/login/LoginComp.tsx
@@ -1,12 +1,16 @@
+import { loginAction } from "@/store/login/login-slice";
 import { alertAction } from "@/store/modal/alert-slice";
+import axios from "axios";
 import { useRouter } from "next/router";
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 
 const LoginComp = () => {
   // const [emailInput, setEmailInput] = useState("test@email.com");
   // const [passwordInput, setPasswordInput] = useState("test123!!");
   const router = useRouter();
+
+  const dispatch = useDispatch();
 
   const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
@@ -17,6 +21,64 @@ const LoginComp = () => {
 
   const passwordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPasswordInput(e.target.value);
+  };
+
+  // 비밀번호 입력 영역에서 Enter 키 누르면 자동 로그인 버튼 클릭로직
+  const autoLoginHandler = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loginBtn();
+    }
+  };
+
+  // 각 입력값에 대한 정합성 확인
+  const validationCheck = () => {
+    if (!emailInput) {
+      dispatch(alertAction.openModal({ cont: "이메일이 입력되지 않았습니다" }));
+      return false;
+    }
+    if (!passwordInput) {
+      dispatch(
+        alertAction.openModal({ cont: "비밀번호가 입력되지 않았습니다" }),
+      );
+      return false;
+    }
+    return true;
+  };
+
+  // 회원가입 버튼 클릭 -> 회원가입 로직 실행
+  const loginBtn = async () => {
+    // 정홥성 확인이 완료되면 '회원가입'로직 실행
+    if (validationCheck()) {
+      let data = {
+        email: emailInput,
+        password: passwordInput,
+      };
+      axios
+        .post("/api/acct/login", data)
+        .then((response) => {
+          console.log("login 결과");
+          console.log(response.data);
+          if (response.data.code === 200) {
+            // getNewPingList(response);
+            const loginData = response.data.data;
+            dispatch(
+              loginAction.login({
+                userEmail: emailInput,
+                // userName: nameInput,
+                accessToken: loginData.accessToken,
+                refreshToken: loginData.refreshToken,
+              }),
+            );
+            dispatch(alertAction.openModal({ cont: response.data.message }));
+            router.push("/");
+          } else {
+            dispatch(alertAction.openModal({ cont: response.data.message }));
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
   };
 
   return (
@@ -38,9 +100,11 @@ const LoginComp = () => {
               value={passwordInput}
               data-key="passwordInput"
               onChange={passwordChangeHandler}
-              // onKeyDown={autoLoginHandler}
+              onKeyDown={autoLoginHandler}
             />
-            <button className="h-11">로그인</button>
+            <button className="h-11" onClick={loginBtn}>
+              로그인
+            </button>
             <p
               className="w-[340px] h-3.5 text-xs text-right text-black cursor-pointer"
               onClick={() => {
