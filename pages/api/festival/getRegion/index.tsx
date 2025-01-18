72fc3f149d552c6157d9f992c2e3d166484216ec
moonctp <moonctp24@gmail.com>
2025-01-18 15:31:57 +0900

    개최지역 조회 API

---
+++ pages/api/festival/getRegion/index.tsx
@@ -0,0 +1,29 @@
+// pages/api/festivals.js
+import axios, { AxiosError } from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "GET") {
+    try {
+      const response = await axios.get(
+        `http://ec2-3-34-40-99.ap-northeast-2.compute.amazonaws.com/location`,
+        { headers: req.headers },
+      );
+      res.status(200).json(response.data);
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        res
+          .status(error.response?.status || 500)
+          .json({ message: "Error fetching data" });
+      } else {
+        res.status(500).json({ message: "Unknown error occurred" });
+      }
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
