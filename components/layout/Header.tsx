6c294003366f711183bf387b44c8353103e6b021
moonctp <moonctp24@gmail.com>
2025-02-02 10:29:39 +0900

    PC Mode Header link connect

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
 import HamComp from "../hamburger/HamComp";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store";
+import axios from "axios";
+import { loginAction } from "@/store/login/login-slice";
+import { alertAction } from "@/store/modal/alert-slice";
 
 const Header = () => {
   const [hamOpen, setHamOpen] = useState(false);
   const router = useRouter();
+  const dispatch = useDispatch();
 
   /**
    * Open Hamburger
@@ -21,6 +27,62 @@ const Header = () => {
     document.body.style.overflow = "unset";
   };
 
+  const isLoginYN = useSelector((state: RootState) => state.login.isLogin);
+  const token2 = useSelector((state: RootState) => state.login.accessToken);
+  //  const userName = useSelector((state: RootState) => state.login.userName);
+  //  const userEmail = useSelector((state: RootState) => state.login.userEmail);
+
+  const [isLogin, setIsLogin] = useState(isLoginYN);
+  const [token, setToken] = useState(token2);
+  //  const [name, setName] = useState(userName);
+  //  const [email, setEmail] = useState(userEmail);
+
+  useEffect(() => {
+    const realLoginYN = localStorage.getItem("isLogin") === "Y";
+    const token3 = localStorage.getItem("accessToken") || "null";
+    //  const name2 = localStorage.getItem("userName") || "null";
+    //  const email2 = localStorage.getItem("userEmail") || "null";
+
+    setIsLogin(realLoginYN);
+    setToken(token3);
+    //  setName(name2);
+    //  setEmail(email2);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoginYN, token2]);
+
+  const goLogin = () => {
+    router.push("/acct/login");
+  };
+  const goSignup = () => {
+    router.push("/acct/signup");
+  };
+  const goMypage = () => {
+    router.push("/acct/mypage");
+  };
+  const goLogout = () => {
+    const data = {
+      token: token,
+    };
+    axios
+      .post("/api/acct/logout", data)
+      .then((response) => {
+        console.log("logout 결과");
+        console.log(response.data);
+        if (response.data.code === 200) {
+          // getNewPingList(response);
+          // const loginData = response.data.data;
+          dispatch(loginAction.logout());
+          // dispatch(alertAction.openModal({ cont: response.data.message }));
+          router.push("/");
+        } else {
+          dispatch(alertAction.openModal({ cont: response.data.message }));
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -29,17 +91,33 @@ const Header = () => {
             LOGO
           </a>
         </div>
-        <ul className="navbar__icons">
-          <li>
-            <button className="acctBtn">로그인</button>
-          </li>
-          <li>
-            <button className="acctBtn">로그아웃</button>
-          </li>
-          <li>
-            <button className="acctBtn">내정보</button>
-          </li>
-        </ul>
+        {isLogin ? (
+          <ul className="navbar__icons">
+            <li>
+              <button className="acctBtn" onClick={goLogin}>
+                로그인
+              </button>
+            </li>
+            <li>
+              <button className="acctBtn" onClick={goSignup}>
+                회원가입
+              </button>
+            </li>
+          </ul>
+        ) : (
+          <ul className="navbar__icons">
+            <li>
+              <button className="acctBtn" onClick={goLogout}>
+                로그아웃
+              </button>
+            </li>
+            <li>
+              <button className="acctBtn" onClick={goMypage}>
+                내정보
+              </button>
+            </li>
+          </ul>
+        )}
         {/* 햄버거 */}
         <svg
           width="35"
